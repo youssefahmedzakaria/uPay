2f3427852ac05c9d1b42f3c467ad5bbb5daa666f
Zeyad-ElNaggar <100488223+Zeyad-ElNaggar@users.noreply.github.com>
2023-11-14 19:30:23 +0200

    added some defensive to register

--- src/App/Register.java
+++ src/App/Register.java
@@ -31,9 +31,10 @@ public class Register {
         System.out.println("Enter your username");
         Scanner in = new Scanner(System.in);
         String user = in.nextLine();
-        in.nextLine();
+//        in.nextLine();
         boolean isTaken = false;
         do {
+            isTaken=false;
             for (int i = 0; i < listOfRegisteredUser.size(); i++) {
                 if (listOfRegisteredUser.get(i).getUsername().equals(user)) {
                     System.out.println("The username you entered is already taken, Try Again");
@@ -49,60 +50,74 @@ public class Register {
         //Password Check
         System.out.println("Enter your passsword");
         String pass = in.nextLine();
-        in.nextLine();
+//        in.nextLine();
         boolean isValid = true;
         do {
             isValid = isPasswordValid(pass);
             if (!isValid) {
                 pass = in.nextLine();
-                in.nextLine();
+//                in.nextLine();
             }
         } while (!isValid);
         System.out.println("Password is added successfully");
 
 
         //Phone Check
-        boolean isPhoneValid=true;
-
+        boolean isPhoneValid;
         System.out.println("Enter your phone number");
         String phone;
-        int phoneNumber;
-        do {
-            isPhoneValid=true;
-            phone = in.nextLine();
-            phoneNumber = Integer.parseInt(phone);
-            in.nextLine();
+        int phoneNumber = 0;
 
-            isPhoneValid = isPhoneValid(phone); //check if code matches regex and returns true
-            if (isPhoneValid){
-                for (User currentUser:listOfRegisteredUser) {
-                    if (phoneNumber==currentUser.getPhoneNum()) {
-                        System.out.println("The phone number you entered is already taken, Try again!");
-                        isPhoneValid = false;
+        do {
+            isPhoneValid = true;
+            phone = in.nextLine();
+
+            try {
+                phoneNumber = Integer.parseInt(phone);
+
+                // Check if phone number matches regex
+                if (!isPhoneValid(phone)) {
+                    System.out.println("Invalid input, Try Again!");
+                    isPhoneValid = false;
+                } else {
+                    // Check if phone number is already taken
+                    for (User currentUser : listOfRegisteredUser) {
+                        if (phoneNumber == currentUser.getPhoneNum()) {
+                            System.out.println("The phone number you entered is already taken, Try again!");
+                            isPhoneValid = false;
+                            break; // No need to continue checking if found
+                        }
                     }
                 }
-            }else{
-                System.out.println("Invalid input, Try Again!");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, Please enter a valid numeric phone number.");
+                isPhoneValid = false;
             }
+
         } while (!isPhoneValid);
 
         System.out.println("Phone number is added successfully");
 
 
-        //PIN Check
-        boolean isPinValid = true;
+        // PIN Check
+        boolean isPinValid = false;
         System.out.println("Enter your PIN");
         String PIN;
+
         do {
             isPinValid = true;
             PIN = in.nextLine();
-            //in.nextLine();
-
-            if (PIN.length() != 4) {
+            try {
+                if (PIN.length() != 4 || !PIN.matches("\\d+")) {
+                    isPinValid = false;
+                    System.out.println("Your PIN must have 4 numeric digits");
+                }
+            } catch (NumberFormatException e) {
                 isPinValid = false;
-                System.out.println("Your PIN must have 4 digits");
+                System.out.println("Invalid input. Please enter a valid numeric PIN.");
             }
         } while (!isPinValid);
+
         System.out.println("Your PIN is added successfully!");
         int pin = Integer.parseInt(PIN);
 
@@ -111,27 +126,34 @@ public class Register {
         System.out.println("Choose your account type\n1-Bank Account\n2-Wallet Account");
         boolean validInput = true;
         do {
-            validInput=true;
-            int choose = in.nextInt();
-            in.nextLine();
+            try {
 
-            if (choose == 1) {
-                BankUser bankUser = new BankUser(user, pass, phoneNumber, pin, "Bank");
-                BankUser updatedBankUser = validateBankUser(bankUser);
+                validInput = true;
+                int choose = in.nextInt();
+                in.nextLine();
 
-                listOfRegisteredUser.add(updatedBankUser);
-                listOfBankUser.add(updatedBankUser);
-                return true;
+                if (choose == 1) {
+                    BankUser bankUser = new BankUser(user, pass, phoneNumber, pin, "Bank");
+                    BankUser updatedBankUser = validateBankUser(bankUser);
 
-            } else if (choose == 2) {
-                WalletUser walletUser = new WalletUser(user, pass, phoneNumber, pin, "Wallet");
-                WalletUser updatedWalletUser = validateWalletUser(walletUser);
+                    listOfRegisteredUser.add(updatedBankUser);
+                    listOfBankUser.add(updatedBankUser);
+                    return true;
 
-                listOfRegisteredUser.add(updatedWalletUser);
-                listOfWalletUser.add(updatedWalletUser);
-                return true;
-            }else {
-                System.out.println("Invalid input, Try Again!");
+                } else if (choose == 2) {
+                    WalletUser walletUser = new WalletUser(user, pass, phoneNumber, pin, "Wallet");
+                    WalletUser updatedWalletUser = validateWalletUser(walletUser);
+
+                    listOfRegisteredUser.add(updatedWalletUser);
+                    listOfWalletUser.add(updatedWalletUser);
+                    return true;
+                } else {
+                    System.out.println("Invalid input, Try Again!");
+                    validInput = false;
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                in.nextLine(); // Consume the invalid input
                 validInput=false;
             }
         }while (!validInput);
@@ -175,22 +197,28 @@ public class Register {
         System.out.println("Choose which bank is your:\n1-NBE\n2-QNB\n3-CIB");
         boolean isValid = true;
         do {
-            isValid = true;
-            in = new Scanner(System.in);
-            int choose = in.nextInt();
-            in.nextLine();
+            try {
+                isValid = true;
+                in = new Scanner(System.in);
+                int choose = in.nextInt();
+                in.nextLine();
 
-            if (choose == 1)
-                user.bank.setBankName("NBE");
-            else if (choose == 2)
-                user.bank.setBankName("QNB");
-            else if (choose == 3)
-                user.bank.setBankName("CIB");
-            else {
-                System.out.println("Invalid input,Try Again!");
-                isValid = false;
+                if (choose == 1)
+                    user.bank.setBankName("NBE");
+                else if (choose == 2)
+                    user.bank.setBankName("QNB");
+                else if (choose == 3)
+                    user.bank.setBankName("CIB");
+                else {
+                    System.out.println("Invalid input,Try Again!");
+                    isValid = false;
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                in.nextLine(); // Consume the invalid input
+                isValid=false;
             }
-        } while (!isValid);
+        }while (!isValid);
 
         //CardNum Check
         String cardNum;
@@ -200,7 +228,7 @@ public class Register {
             isCardNumValid=true;
 
             cardNum = in.nextLine();
-            in.nextLine();
+//            in.nextLine();
 
             String cardNumRegex = "^\\d{16}$";
 
@@ -225,7 +253,7 @@ public class Register {
         do {
             isDateValid = true;
             expiryDate = in.nextLine();
-            in.nextLine();
+//            in.nextLine();
             String expiryDateRegex = "^(0[1-9]|1[0-2])/\\d{2}$";
 
             if (!expiryDate.matches(expiryDateRegex)) {
@@ -241,7 +269,7 @@ public class Register {
         boolean isAccountNumValid = true;
         do {
             accountNum = in.nextLine();
-            in.nextLine();
+//            in.nextLine();
 
             String bankAccountRegex = "^\\d{10}$";
 
@@ -285,31 +313,41 @@ public class Register {
         float randomBalance = 10000 + random.nextFloat() * (20000 - 10000);
         float userBalance=randomBalance;
 
-//        user.walletProvider.setBalance(randomBalance);
 
-        //Wallet Provider
+        // Wallet Provider
         System.out.println("Choose your Wallet Provider:");
         System.out.println("1-Mobile Wallet (Vodafone Cash)");
         System.out.println("2-Bank Wallet (EGBank)");
         System.out.println("3-E-Payment Wallet (Telda)");
 
-        boolean isWalletValid=true;
+        boolean isWalletValid = false;
         int choose;
         do {
-            isWalletValid=true;
-            choose=in.nextInt();
-            if (choose==1){
-                user.walletProvider=new MobileWallet(userBalance,"MobileWallet", "VodafoneCash");
-            }else if (choose==2){
-                user.walletProvider=new BankWallet(userBalance,"BankWallet", "EGBank");
-            }else if (choose==3){
-                user.walletProvider=new ePaymentWallet(userBalance,"E-PaymentWallet", "Telda");
-            }else {
-                System.out.println("Wrong input, Try Again!");
-                isWalletValid=false;
-            }
-        }while (!isWalletValid);
+            try {
+                isWalletValid = true;
+                choose = in.nextInt();
+                in.nextLine(); // Consume the newline character
 
+                switch (choose) {
+                    case 1:
+                        user.walletProvider = new MobileWallet(userBalance, "MobileWallet", "VodafoneCash");
+                        break;
+                    case 2:
+                        user.walletProvider = new BankWallet(userBalance, "BankWallet", "EGBank");
+                        break;
+                    case 3:
+                        user.walletProvider = new ePaymentWallet(userBalance, "E-PaymentWallet", "Telda");
+                        break;
+                    default:
+                        System.out.println("Wrong input, Try Again!");
+                        isWalletValid = false;
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                isWalletValid=false;
+                in.nextLine(); // Consume the invalid input
+            }
+        } while (!isWalletValid);
 
         validateOTP();
 
@@ -326,7 +364,7 @@ public class Register {
             System.out.println("Enter your OTP");
             Scanner in = new Scanner(System.in);
             inputOTP = in.nextLine();
-            in.nextLine();
+//            in.nextLine();
             if (inputOTP.equals(OTP))
                 return true;
             else {
